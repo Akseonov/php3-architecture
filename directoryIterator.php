e3ff06e244724ff0d18e2f97f2d407fdf6ae173f
akseonov <k.aksenov@legacystudio.ru>
2022-10-08 16:42:44 +0500

    lesson_9 сделал дз к 9 уроку

--- directoryIterator.php
+++
@@ -1,8 +0,0 @@
-<?php
-//вывод всего содержимого папки вверху проекта
-foreach (new DirectoryIterator('../') as $fileInfo) {
-    if ($fileInfo->isDot()) {
-        continue;
-    }
-    echo $fileInfo->getFilename() . PHP_EOL;
-}
